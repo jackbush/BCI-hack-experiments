88d4b7c6b50818225f4ff7160a32611ada3048d5
Jack Bush <bush.jack@gmail.com>
2015-03-24 20:48:05 +0000

    added api serving on LH 9000 to eeg query script

--- get_eeg_feed.rb
+++ get_eeg_feed.rb
@@ -4,6 +4,7 @@ require 'artoo'
 
 connection :neurosky, :adaptor => :neurosky, :port => '/dev/tty.MindWave'
 device :neurosky, :driver => :neurosky, :interval => 0.1
+api :host => '127.0.0.1', :port => '9000'
 
 def handle_eeg(sender, data)
   puts data
