6faac96dfb5e8bdeaa39ac20718c515d3a9c38dd
Jack Bush <bush.jack@gmail.com>
2015-03-25 20:47:43 +0000

    have eeg feed working with json

--- Gemfile
+++ Gemfile
@@ -1,7 +1,5 @@
-# A sample Gemfile
 source "https://rubygems.org"
 
-# gem "rails"
-
-# gem 'artoo'
+gem 'artoo'
 gem 'artoo-neurosky'
+gem 'json'
--- eeg_feed.rb
+++ eeg_feed.rb
@@ -1,13 +1,14 @@
 # DOCUMENTATION http://artoo.io/documentation/platforms/neurosky/
 
 require 'artoo'
+require 'json'
 
 connection :neurosky, :adaptor => :neurosky, :port => '/dev/tty.MindWave'
 device :neurosky, :driver => :neurosky, :interval => 0.1
 api :host => '127.0.0.1', :port => '9000'
 
 def handle_eeg(sender, data)
-  puts data
+  puts data.to_json
 end
 
 work do
