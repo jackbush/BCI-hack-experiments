7c19463acea890810e682ec162115e2173de3a0b
Jack Bush <bush.jack@gmail.com>
2015-03-26 08:36:43 +0000

    broke out documentation to own md file

--- query_terminal_app.rb
+++ query_terminal_app.rb
@@ -66,25 +66,3 @@ gets
 response = menu
 
 end
-
-# attention(data)
-# Event with the user's current attention level.
-
-# blink(data)
-# Event with the user's current blink level.
-
-# meditation(data)
-# Event with the user's current meditation level.
-
-# signal_quality(data)
-# Event showing signal strength.
-
-# wave(data)
-# Event showing wave data.
-
-# eeg(data)
-# Event showing EEG data.
-# { :delta=>7023617, :theta=>15294464, :lo_alpha=>15209472, :hi_alpha=>13321984, :lo_beta=>4527616, :hi_beta=>12073472, :lo_gamma=>862464, :mid_gamma=>13637632 }
-
-# start
-# Gets triggered when the Mindwave is started and ready to be used.
