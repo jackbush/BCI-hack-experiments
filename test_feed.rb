a52ca646397baafec37bb9571e608c8a03c96a22
Jack Bush <bush.jack@gmail.com>
2015-03-26 17:10:11 +0000

    made composite feed

--- test_feed.rb
+++ test_feed.rb
@@ -5,7 +5,8 @@ device :neurosky, :driver => :neurosky, :interval => 0.1
 api :host => '127.0.0.1', :port => '9000'
 
 def handle_eeg(sender, data)
-  puts "#{sender} | #{data}"
+  name = sender.split('_').last[0..2]
+  puts "#{name} | #{data}"
 end
 
 work do
