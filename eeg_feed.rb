b583ff856db18c306486111636d2ef454b0e4d9a
Jack Bush <bush.jack@gmail.com>
2015-03-26 17:22:22 +0000

    cleaned out broken hosts from feeds, targetted terminal app as a refactoring proof of concept

--- eeg_feed.rb
+++ eeg_feed.rb
@@ -1,11 +1,7 @@
-# DOCUMENTATION http://artoo.io/documentation/platforms/neurosky/
-
 require 'artoo'
-require 'json'
 
 connection :neurosky, :adaptor => :neurosky, :port => '/dev/tty.MindWave'
 device :neurosky, :driver => :neurosky, :interval => 0.1
-api :host => '127.0.0.1', :port => '9000'
 
 def handle_eeg(sender, data)
   puts data
